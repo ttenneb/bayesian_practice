cc510909560e1ed70442a28de96a47a174493f8f
ttenneb <bcg77@scarletmail.rutgers.edu>
2024-04-10 14:57:17 -0700

    Working verison

--- particle_sim.py
+++ particle_sim.py
@@ -11,14 +11,17 @@ import matplotlib.pyplot as plt
 pygame.init()
 
 # Constants
-SCREEN_WIDTH, SCREEN_HEIGHT = 800, 800
+SCREEN_WIDTH, SCREEN_HEIGHT = 1200, 1200
 RUN_LIMIT = 5000
 FPS = 320
-lr = .12
+lr = .01
 steps = 200
-entropy_norm = 10
-batch_size = 50
-gravity_constant = 50
+entropy_norm = 15
+batch_size = 100
+gravity_constant = 125
+var_min = 0.0001
+
+epsilon = 1e-6
 # Set up the screen
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Orbit")
@@ -35,7 +38,7 @@ class GameObject:
 
     def update(self):
         self.position = self.position + self.velocity
-        self.position.clamp(min=-5000, max=5000)
+        # self.position = torch.clamp(self.position, min = -1500, max = 1500)
     def apply_force(self, force):
         self.velocity = self.velocity + force
 
@@ -49,7 +52,7 @@ def orbit_quality(object1, stationary_positions):
     normals = torch.stack([-velocities[:, 1], velocities[:, 0]], dim=1)
 
     # Calculate vectors from moving particles (current positions) to stationary particles
-    epsilon = 1e-8
+    
     
     stationary_vectors = (stationary_positions.detach() - object1.position)
 
@@ -90,27 +93,38 @@ trajectory_best = []
 elbo_best = 10.0
 epsilon = 1e-6
 
-fx_a, fx_v = torch.tensor([epsilon], requires_grad=True), torch.tensor([0.5], requires_grad=True)
-fy_a, fy_v = torch.tensor([epsilon], requires_grad=True), torch.tensor([0.5], requires_grad=True)
-px_a, px_v = torch.tensor([epsilon], requires_grad=True), torch.tensor([0.66], requires_grad=True)
-py_a, py_v = torch.tensor([epsilon], requires_grad=True), torch.tensor([0.66], requires_grad=True)
+fx_a, fx_v = torch.tensor([0.0], requires_grad=True), torch.tensor([0.5], requires_grad=True)
+fy_a, fy_v = torch.tensor([0.0], requires_grad=True), torch.tensor([0.5], requires_grad=True)
+px_a, px_v = torch.tensor([0.0], requires_grad=True), torch.tensor([0.5], requires_grad=True)
+py_a, py_v = torch.tensor([0.0], requires_grad=True), torch.tensor([0.5], requires_grad=True)
 
 
-optimizer = torch.optim.Adamax([fx_a, fx_v, fy_a, fy_v, px_a, px_v, py_a, py_v], lr=lr)
-# optimizer = torch.optim.Adam([fx_a, fx_v, fy_a, fy_v], lr=lr)
+optimizer = torch.optim.SGD([fx_a, fx_v, fy_a, fy_v, px_a, px_v, py_a, py_v], lr=lr)
+# optimizer = torch.optim.SGD([fx_a, fx_v, fy_a, fy_v], lr=lr)
 distances = []
-torch.autograd.set_detect_anomaly(True)
+torch.autograd.set_detect_anomaly(False)
 try:
     for _ in range(steps):
+        print("Simulating with params:")
+        print("fx_a:", float(fx_a))
+        print("fx_v:", float(fx_v))
+        print("fy_a:", float(fy_a))
+        print("fy_v:", float(fy_v))
+        print("px_a:", float(px_a))
+        print("px_v:", float(px_v))
+        print("py_a:", float(py_a))
+        print("py_v:", float(py_v))
+
+
         trajectory = []
         orbit_tick = torch.tensor([0.0], requires_grad=True)
-        in_range_tick = torch.tensor([0.0], requires_grad=True)
-        velocity_x = dist.Normal(fx_a, (fx_v+epsilon)**2)
-        velocity_y = dist.Normal(fy_a, (fy_v+epsilon)**2)
+        in_range_tick = torch.tensor([1.0], requires_grad=False)
+        velocity_x = dist.Normal(fx_a, (fx_v+var_min)**2)
+        velocity_y = dist.Normal(fy_a, (fy_v+var_min)**2)
 
 
-        pos_x =  dist.Normal((px_a)*SCREEN_WIDTH/2, (px_v ** 2)* 200)
-        pos_y =  dist.Normal((py_a)*SCREEN_HEIGHT/2, (py_v ** 2)* 200)
+        pos_x =  dist.Normal((px_a)*SCREEN_WIDTH/2, ((px_v+var_min)**2)* 200)
+        pos_y =  dist.Normal((py_a)*SCREEN_HEIGHT/2,((py_v+var_min)**2)* 200)
         initial_velocity =  torch.stack([velocity_x.rsample((batch_size,)), velocity_y.rsample((batch_size,))], dim=-1).to(dtype=torch.float32)
         # initial_velocity =  torch.stack([torch.zeros((batch_size, 1)), torch.zeros((batch_size, 1))], dim=-1).to(dtype=torch.float32)
         start_x = torch.reshape(center_x, (batch_size, 1)) + torch.reshape(pos_x.rsample((batch_size,1)), (batch_size, 1))
@@ -118,7 +132,7 @@ try:
         objects = GameObject(start_x, start_y, 5, 5, initial_velocity)
         center = GameObject(center_x, center_y, 5, 5, torch.tensor([0.0, 0.0], dtype=torch.float32, requires_grad=False))
         last_path_point = objects.position
-    
+
         if _ == 0:
             fx_a_start, fx_v_start = float(fx_a), float(fx_v)
             fy_a_start, fy_v_start = float(fy_a), float(fy_v)
@@ -128,18 +142,14 @@ try:
         while True:
             # Update game state
             objects.update()
-            epsilon = 1e-6
             # Revolve around center
             distance = center.position.detach() - objects.position 
-            if(torch.any(distance) == 0):
-                print("ZEROSSS")
-            if(torch.all(distance) != 0):
-                norms = torch.norm(distance, p=2, dim=1, keepdim=True)
-                # You may adjust this constant as needed
-                
-                gravity = (gravity_constant / (norms**2 + epsilon)) * (distance / (norms + epsilon)) + epsilon
 
-                objects.apply_force(gravity)
+            norms = torch.norm(distance, p=2, dim=1, keepdim=True) 
+            # You may adjust this constant as needed
+            
+            gravity = (gravity_constant / ((norms.detach())**2 + epsilon)) * (distance / (norms.detach())) + epsilon
+            objects.apply_force(gravity)
 
             
 
@@ -157,14 +167,16 @@ try:
                 else:
                     out_orbit_count += 1
             
-            orbit_tick = orbit_tick + (((orbit_count)**2)*(orbit_quality(objects, center.position)))*(in_range_tick/RUN_LIMIT)
-            
+            orbit_tick = orbit_tick + (orbit_quality(objects, center.position))*((in_range_tick/RUN_LIMIT))
+
+                
+
             done = False
             if in_range_tick > RUN_LIMIT:
                 done = True
             if done:
                 print("simulation done")
-
+            
                 # RENDER EVERY STEP
                 for p in trajectory:
                     # Handle events
@@ -189,74 +201,66 @@ try:
                     # Cap the frame rate
                     clock.tick(FPS)
                 break
-        print("ORBIT TICK: ", orbit_tick, "RUN LIMIT: ", RUN_LIMIT, "BATCH SIZE: ", batch_size)
-        likelihood = torch.log((orbit_tick/RUN_LIMIT)/batch_size)
+        print("batch done", _ / steps)
+
+        likelihood = torch.log((orbit_tick/RUN_LIMIT) + epsilon)
         entropy = (pos_x.entropy() + pos_y.entropy() + velocity_x.entropy() + velocity_y.entropy())/entropy_norm
 
         elbo = -1*likelihood - entropy 
-        print("Params:")
-        print("fx_a:", float(fx_a))
-        print("fx_v:", float(fx_v))
-        print("fy_a:", float(fy_a))
-        print("fy_v:", float(fy_v))
-        print("px_a:", float(px_a))
-        print("px_v:", float(px_v))
-        print("py_a:", float(py_a))
-        print("py_v:", float(py_v))
-
-        print("Objectives:  log Likelihood: " + str(float(-1*likelihood)), "    Entropy: " + str(float(entropy)), "   Negative ELBO: " + str(float(elbo)))
-        orbit_tick.retain_grad()
-        in_range_tick.retain_grad()
-        elbo.retain_grad()
-        elbo.backward()
         
+
+        # orbit_tick.retain_grad()
+        elbo.backward()
+        print("Objectives:  log Likelihood: " + str(float(-1*likelihood)), "    Entropy: " + str(float(entropy)), "   Negative ELBO: " + str(float(elbo)))
+        print("RAW GRADS:   ", fx_a.grad, fy_a.grad, fx_v.grad, fy_v.grad, px_a.grad, py_a.grad, px_v.grad, py_v.grad)
+        # print("RAW GRADS:   ", fx_a.grad, fy_a.grad, fx_v.grad, fy_v.grad)
         # Gradient clipping
-        # torch.nn.utils.clip_grad_norm_(fx_a, 1)
-        # torch.nn.utils.clip_grad_norm_(fy_a, 1)
-        # torch.nn.utils.clip_grad_norm_(fx_v, 1)
-        # torch.nn.utils.clip_grad_norm_(fy_v, 1)
-        # torch.nn.utils.clip_grad_norm_(px_a, 1)
-        # torch.nn.utils.clip_grad_norm_(py_a, 1)
-        # torch.nn.utils.clip_grad_norm_(px_v, 1)
-        # torch.nn.utils.clip_grad_norm_(py_v, 1)
-        print(fx_a.grad, fy_a.grad, fx_v.grad, fy_v.grad, px_a.grad, py_a.grad, px_v.grad, py_v.grad)
-        max_grad = max([fx_a.grad, fx_v.grad, fy_a.grad, fy_v.grad, px_a.grad, px_v.grad,py_a.grad, py_v.grad])
-        min_grad = min([fx_a.grad, fx_v.grad, fy_a.grad, fy_v.grad, px_a.grad, px_v.grad,py_a.grad, py_v.grad])
+        # torch.nn.utils.clip_grad_norm_(fx_a, 10)
+        # torch.nn.utils.clip_grad_norm_(fy_a, 10)
+        # torch.nn.utils.clip_grad_norm_(fx_v, 10)
+        # torch.nn.utils.clip_grad_norm_(fy_v, 10)
+        # torch.nn.utils.clip_grad_norm_(px_a, 10)
+        # torch.nn.utils.clip_grad_norm_(py_a, 10)
+        # torch.nn.utils.clip_grad_norm_(px_v, 10)
+        # torch.nn.utils.clip_grad_norm_(py_v, 10)
+        # print("CLIPPED GRADS:   ", fx_a.grad, fy_a.grad, fx_v.grad, fy_v.grad, px_a.grad, py_a.grad, px_v.grad, py_v.grad)
+        # Gradoemt Norm/Stand
+        # grad_list = np.array([fx_a.grad, fx_v.grad, fy_a.grad, fy_v.grad, px_a.grad, px_v.grad,py_a.grad, py_v.grad])
+        # grad_list = np.array([fx_a.grad, fx_v.grad, fy_a.grad, fy_v.grad])
+        # mean = sum(grad_list)/len(grad_list)
+        # sd = math.sqrt((sum((grad_list - mean)**2)/len(grad_list))[0])
+        
+        # print("Norm Params:     ", mean, sd)
+        # if mean != 0 and sd != 0:
+        #     fx_v.grad = ((fx_v.grad - mean)/sd)
+        #     fx_a.grad = ((fx_a.grad - mean)/sd)
+        #     fy_a.grad = ((fy_a.grad - mean)/sd)
+        #     fy_v.grad = ((fy_v.grad - mean)/sd)
+        #     px_a.grad = ((px_a.grad - mean)/sd)
+        #     px_v.grad = ((px_v.grad - mean)/sd)
+        #     py_a.grad = ((py_a.grad - mean)/sd)
+        #     py_v.grad = ((py_v.grad - mean)/sd)
+        # print("STAND GRADS:   ", fx_a.grad, fy_a.grad, fx_v.grad, fy_v.grad, px_a.grad, py_a.grad, px_v.grad, py_v.grad)
+        # print("STAND GRADS:   ", fx_a.grad, fy_a.grad, fx_v.grad, fy_v.grad)
 
-        fx_v.grad = (fx_v.grad/max_grad)
-        fx_a.grad = (fx_a.grad/max_grad)
-        fy_a.grad = (fy_a.grad/max_grad)
-        fy_v.grad = (fy_v.grad/max_grad)
-        px_a.grad = (px_a.grad/max_grad)
-        px_v.grad = (px_v.grad/max_grad)
-        py_a.grad = (py_a.grad/max_grad)
-        py_v.grad = (py_v.grad/max_grad)
-        # norm = max([torch.abs(fx_a.grad), torch.abs(fx_v.grad), torch.abs(fy_a.grad), torch.abs(fy_v.grad)])
-        # fx_a.grad = fx_a.grad / norm
-        # fx_v.grad = fx_v.grad / norm
-        # fy_a.grad = fx_a.grad / norm
-        # fy_v.grad = fx_v.grad / norm
+        if(not torch.isnan(fx_a.grad).any()):
+            # Inside your game loop or after certain iterations
+            optimizer.step() 
+            if elbo.detach() < elbo_best:
+                elbo_best = elbo.detach().clone()
+                trajectory_best = trajectory.copy()
 
-        print(fx_a.grad, fy_a.grad, fx_v.grad, fy_v.grad, px_a.grad, py_a.grad, px_v.grad, py_v.grad)
-        print(orbit_tick.grad, in_range_tick.grad)
-        print(elbo.grad)
-        # Inside your game loop or after certain iterations
-        optimizer.step() 
-        if elbo.detach() < elbo_best:
-            elbo_best = elbo.detach().clone()
-            trajectory_best = trajectory.copy()
-
-        elbo_list.append(float(elbo.detach()))
+            elbo_list.append(float(elbo.detach()))
         optimizer.zero_grad()  # Clear gradients for the next iteration
-        print("batch done", _ / steps)
 except Exception as e:
-    print("Training failed")
+    print("Training failed: ")
     # raise e
 
 time_points = list(range(len(elbo_list)))
 
 # Create the plot
 FPS = 260
+
 
 plot_normal(fx_a_start, fx_v_start**2, float(fx_a), float(fx_v**2))
 
